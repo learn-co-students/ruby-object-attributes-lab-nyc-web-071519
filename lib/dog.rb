4b668db6241beed75666a4df3fdfdd741f5b4c45
Andrew Santos <asantos@alumni.nd.edu>
2019-07-17 14:48:57 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,47 @@
+class Dog
+
+    def initialize
+        @name
+        @breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name= (new_name)
+        @name = new_name
+    end
+
+    def breed= (new_breed)
+        @breed = new_breed
+    end
+end
+
+class Person
+
+    def initialize
+        @name
+        @job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
+
+    def name= (new_name)
+        @name = new_name
+    end
+
+    def job= (new_job)
+        @job = new_job
+    end
+end
